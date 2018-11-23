f2139d8e8470f923981f5cce43ebf91f7ce0924d
Gkemon <emon.info2013@gmail.com>
2018-11-24 00:21:10 +0600

    Add continuesous listening

--- app/src/main/java/gkemon/binarygeek/com/voicetotext/MainActivity.java
+++ app/src/main/java/gkemon/binarygeek/com/voicetotext/MainActivity.java
@@ -2,9 +2,12 @@ package gkemon.binarygeek.com.voicetotext;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -17,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtSpeechInput;
     private ImageButton btnSpeak;
     private final int REQ_CODE_SPEECH_INPUT = 100;
+    public static String TAG="GK";
+
+    private SpeechRecognizer sr;
+
+    private SpeechRecognizerManager mSpeechManager;
+
+
 
 
     @Override
@@ -29,16 +39,47 @@ public class MainActivity extends AppCompatActivity {
         btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
 
 
+
+
+
+
         btnSpeak.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                promptSpeechInput();
+                //promptSpeechInput();
+                //promptSpeechInputWithOUTDialog();
+
+
+                if(mSpeechManager==null)
+                {
+                    SetSpeechListenerForContinuesListening();
+                }
+                else if(!mSpeechManager.ismIsListening())
+                {
+                    mSpeechManager.destroy();
+                    SetSpeechListenerForContinuesListening();
+                }
+
+
             }
         });
 
     }
 
-    private void promptSpeechInput() {
+
+    //with out dialog
+    public void promptSpeechInputWithOUTDialog(){
+
+        sr = SpeechRecognizer.createSpeechRecognizer(this);
+        sr.setRecognitionListener(new listener());
+        sr.startListening(new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH));
+    }
+
+
+    //With dialog
+    private void promptSpeechInputWithDialog() {
+
+
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
@@ -73,4 +114,116 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+
+    class listener implements RecognitionListener
+    {
+        public void onReadyForSpeech(Bundle params)
+        {
+            Log.d(TAG, "onReadyForSpeech");
+        }
+        public void onBeginningOfSpeech()
+        {
+            Log.d(TAG, "onBeginningOfSpeech");
+        }
+        public void onRmsChanged(float rmsdB)
+        {
+            Log.d(TAG, "onRmsChanged");
+        }
+        public void onBufferReceived(byte[] buffer)
+        {
+            Log.d(TAG, "onBufferReceived");
+        }
+        public void onEndOfSpeech()
+        {
+            Log.d(TAG, "onEndofSpeech");
+        }
+        public void onError(int error)
+        {
+            Log.d(TAG,  "error " +  error);
+
+        }
+        public void onResults(Bundle results)
+        {
+            String str = new String();
+            Log.d(TAG, "onResults " + results);
+            ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for (int i = 0; i < data.size(); i++)
+            {
+                Log.d(TAG, "result " + data.get(i));
+                str += data.get(i);
+            }
+            Log.d(TAG,"results: "+String.valueOf(data.size()));
+        }
+        public void onPartialResults(Bundle partialResults)
+        {
+            Log.d(TAG, "onPartialResults");
+        }
+        public void onEvent(int eventType, Bundle params)
+        {
+            Log.d(TAG, "onEvent " + eventType);
+        }
+    }
+
+
+    private void SetSpeechListenerForContinuesListening()
+    {
+        mSpeechManager=new SpeechRecognizerManager(this, new SpeechRecognizerManager.onResultsReady() {
+            @Override
+            public void onResults(ArrayList<String> results) {
+
+
+
+                if(results!=null && results.size()>0)
+                {
+
+                    if(results.size()==1)
+                    {
+                        if(mSpeechManager!=null)
+                        mSpeechManager.destroy();
+
+
+                        //TODO
+                        mSpeechManager = null;
+                        txtSpeechInput.setText(results.get(0));
+                    }
+                    else {
+                        StringBuilder sb = new StringBuilder();
+                        if (results.size() > 5) {
+                            results = (ArrayList<String>) results.subList(0, 5);
+                        }
+                        for (String result : results) {
+                            sb.append(result).append("\n");
+                        }
+                        txtSpeechInput.setText(sb.toString());
+                    }
+                }
+                else
+                    txtSpeechInput.setText("No result found");
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        if(mSpeechManager!=null) {
+            mSpeechManager.destroy();
+            mSpeechManager=null;
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        if(mSpeechManager==null)
+        {
+            SetSpeechListenerForContinuesListening();
+        }
+        else if(!mSpeechManager.ismIsListening())
+        {
+            mSpeechManager.destroy();
+            SetSpeechListenerForContinuesListening();
+        }
+        super.onResume();
+    }
 }
